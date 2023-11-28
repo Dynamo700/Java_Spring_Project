7bd8bd88421c4433fd0ecddb001d5d285f5f0ade
Nick <nickq4504@gmail.com>
2023-11-28 17:46:03 -0330

    -functionality for viewing courses by course id, updating and deleting courses added. -functionality for viewing students by id, updating and deleting students added.

--- src/main/java/com/projectapp/restapi/controller/courseController.java
+++ src/main/java/com/projectapp/restapi/controller/courseController.java
@@ -2,13 +2,11 @@ package com.projectapp.restapi.controller;
 
 import com.projectapp.restapi.models.course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.projectapp.restapi.repo.courseinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class courseController {
@@ -21,7 +19,7 @@ public class courseController {
         return c_interface.findAll();
     }
 
-    @PostMapping(value = "courses/add")
+    @PostMapping(value = "/courses/add")
     public String addCourse(@RequestBody course course) {
         c_interface.save(course);
         return "Course saved!";
@@ -31,5 +29,29 @@ public class courseController {
     public List<course> GetCourseList() {
         return c_interface.findAll();
     }
+    @GetMapping(value = "/courses/{CourseId}")
+    public Optional<course> ViewedCourse(@PathVariable long CourseId) {
+        Optional<course> viewedCourse = c_interface.findById(CourseId);
+        return viewedCourse;
+    }
+    @PutMapping(value = "/courses/update/{CourseId}")
+    public String modifyCourse(@PathVariable long CourseId, @RequestBody course course){
+        course modifiedCourse = c_interface.findById(CourseId).get();
+        modifiedCourse.setCourseId(course.getCourseId());
+        modifiedCourse.setCourseName(course.getCourseName());
+        modifiedCourse.setCapacity(course.getCapacity());
+        modifiedCourse.setCourseNumber(course.getCourseNumber());
+        c_interface.save(modifiedCourse);
+        return "Course updated!";
+    }
+    @DeleteMapping(value = "/courses/delete/{CourseId}")
+    public String deleteCourse(@PathVariable long CourseId){
+        course deleteCourse = c_interface.findById(CourseId).get();
+        c_interface.delete(deleteCourse);
+        return "Deleted the course with id: "+CourseId;
+
+    }
+
+
 
 }
--- src/main/java/com/projectapp/restapi/controller/studentController.java
+++ src/main/java/com/projectapp/restapi/controller/studentController.java
@@ -2,25 +2,22 @@ package com.projectapp.restapi.controller;
 
 import com.projectapp.restapi.models.student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import com.projectapp.restapi.repo.studentinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 
 public class studentController {
 
 
-
     @Autowired
     private studentinterface s_interface;
 
     @GetMapping(value = "/")
-    public String getPage(){
+    public String getPage() {
         return "hello";
     }
 
@@ -29,7 +26,7 @@ public class studentController {
         return s_interface.findAll();
     }
 
-    @PostMapping(value = "student/add")
+    @PostMapping(value = "/student/add")
     public String addStudent(@RequestBody student student) {
         s_interface.save(student);
         return "Student saved!";
@@ -38,6 +35,37 @@ public class studentController {
     @GetMapping(value = "/students/list")
     public List<student> GetStudentList() {
         return s_interface.findAll();
+    }
+    @PutMapping(value = "students/update/{id}")
+    public String modifyStudent(@PathVariable long id, @RequestBody student student){
+        student modifiedStudent = s_interface.findById(id).get();
+        modifiedStudent.setStudentid(student.getStudentid());
+        modifiedStudent.setFirstName(student.getFirstName());
+        modifiedStudent.setLastName(student.getLastName());
+        modifiedStudent.setCity(student.getCity());
+        modifiedStudent.setAddress(student.getAddress());
+        modifiedStudent.setEmail(student.getEmail());
+        modifiedStudent.setPhone(student.getPhone());
+        modifiedStudent.setPostal(student.getPostal());
+        s_interface.save(modifiedStudent);
+        return "Student updated!";
+    }
+
+    @DeleteMapping(value = "students/delete/{id}")
+    public String deleteStudent(@PathVariable long id){
+        student deleteStudent = s_interface.findById(id).get();
+        s_interface.delete(deleteStudent);
+        return "Deleted the course with id: "+id;
+
+    }
+
+
+    @GetMapping(value = "/students/{id}")
+    public Optional<student> ViewStudent(@PathVariable long id){
+        Optional<student> viewedStudent = s_interface.findById(id);
+        return viewedStudent;
+
+
     }
 
 }
